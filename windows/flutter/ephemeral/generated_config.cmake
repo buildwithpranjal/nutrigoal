40a86c79479bd7ba3eb0404e7cb4a74fa4f06092
Pranjal lamsal <pranjallamsal999@gmail.com>
2026-09-11 15:00:09 +0500

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Project\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Project\\Flutter\\nutrigoal" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Project\\Flutter\\flutter"
+  "PROJECT_DIR=D:\\Project\\Flutter\\nutrigoal"
+  "FLUTTER_ROOT=D:\\Project\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Project\\Flutter\\nutrigoal\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Project\\Flutter\\nutrigoal"
+  "FLUTTER_TARGET=D:\\Project\\Flutter\\nutrigoal\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9CVUlMRF9OQU1FPTEuMC4w,RkxVVFRFUl9CVUlMRF9OVU1CRVI9MQ==,RkxVVFRFUl9WRVJTSU9OPTMuNDcuMQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NjY1NTQ4MmVjMA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NWQ1MzE3ODg2OQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMy4x"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Project\\Flutter\\nutrigoal\\.dart_tool\\package_config.json"
+)
